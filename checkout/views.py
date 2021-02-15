cf8c46804f81d3f18a8dbfddf1a7c146881a2eb3
djrowley2104 <djrowley@hotmail.co.uk>
2021-02-15 08:04:17 +0000

    sorting out erros

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
@@ -8,6 +9,23 @@ from products.models import Product
 from bag.contexts import bag_contents
 
 import stripe
+import json
+
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'bag': json.dumps(request.session.get('bag', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
 
 
 def checkout(request):
@@ -30,7 +48,11 @@ def checkout(request):
         }
         order_form = OrderForm(form_data)
         if order_form.is_valid():
-            order = order_form.save()
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_bag = json.dumps(bag)
+            order.save()
             for item_id, item_data in bag.items():
                 try:
                     product = Product.objects.get(id=item_id)
@@ -87,8 +109,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51IIqVcGVQVSxnw1Z0ZCVx9vAJinDpqHTLNT3OkAxHtmR9GqFFqzxZve6lgNdqOqHPFXy5sWF48orrPO53BFudSiA00b099jxOv',
-        'client_secret': 'sk_test_51IIqVcGVQVSxnw1Z6vzOQSWD5IcKiTHVgTNEI9XyBHMxY7QIpD5t9VAXbM23xhUJ6vjjzTLjyggjcCE3xTW4ujLU00i67GuW8Z',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
@@ -110,8 +132,6 @@ def checkout_success(request, order_number):
     template = 'checkout/checkout_success.html'
     context = {
         'order': order,
-        'stripe_public_key': 'pk_test_51IIqVcGVQVSxnw1Z0ZCVx9vAJinDpqHTLNT3OkAxHtmR9GqFFqzxZve6lgNdqOqHPFXy5sWF48orrPO53BFudSiA00b099jxOv',
-        'client_secret': 'sk_test_51IIqVcGVQVSxnw1Z6vzOQSWD5IcKiTHVgTNEI9XyBHMxY7QIpD5t9VAXbM23xhUJ6vjjzTLjyggjcCE3xTW4ujLU00i67GuW8Z',
     }
 
     return render(request, template, context)
